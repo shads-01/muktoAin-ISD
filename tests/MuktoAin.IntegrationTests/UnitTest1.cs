d1ed4ba83f095c58686b9d7f3f9be5753f7b7181
Hrittika Saha <68088694+hrittikaaa@users.noreply.github.com>
2026-08-25 19:26:06 +0600

    chore: add unit and integration test project scaffolds

---
+++ tests/MuktoAin.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace MuktoAin.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/MuktoAin.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace MuktoAin.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
